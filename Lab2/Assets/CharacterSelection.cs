dc8f3af8a39e03007891f67c695b990bf1bb357c
wheraldo <wheraldo@my.centennialcollege.ca>
2017-09-15 23:46:19 -0400

    Lab2
    Uploading Lab2 with colored buttons corresponding to colored sprites.

---
+++ Lab2/Assets/CharacterSelection.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    private GameObject[] characterList;
+    private int index;
+
+    private void Start()
+    {
+        characterList = new GameObject[transform.childCount];
+
+        for(int i = 0; i < transform.childCount; i++)
+            characterList[1] = transform.GetChild(i).gameObject;
+
+        foreach (GameObject g in characterList)
+            g.SetActive(false);
+
+        if (characterList[3])
+            characterList[0].SetActive(true);
+    }
+
+    public void GreyButton()
+    {
+        characterList[index].SetActive(false);
+
+        index--;
+        if (index < 0)
+            index = characterList.Length - 1;
+
+        characterList[index].SetActive(true);
+    }
+
+    public void BlueButton()
+    {
+        characterList[index].SetActive(false);
+
+        index++;
+        if (index == characterList.Length)
+            index = 0;
+
+        characterList[index].SetActive(true);
+    }
+
+    public void GreenButton()
+    {
+        characterList[index].SetActive(false);
+
+        index++;
+        if (index == characterList.Length)
+            index = 3;
+
+        characterList[index].SetActive(true);
+    }
+}
